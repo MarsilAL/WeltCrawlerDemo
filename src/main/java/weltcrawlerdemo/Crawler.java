3ffdc2991025c079b6bb35e23c712896d9bd5e28
Marsil <alashkar.marsil@gmail.com>
2020-03-04 15:28:49 +0100

    #5 implement PingPong endpont

--- src/main/java/weltcrawlerdemo/Crawler.java
+++ src/main/java/weltcrawlerdemo/Crawler.java
@@ -4,10 +4,15 @@ import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import weltcrawlerdemo.domain.ArticleUseCase;
 import weltcrawlerdemo.domain.StorageUseCase;
 import weltcrawlerdemo.infrastructure.*;
 
+import weltcrawlerdemo.infrastructure.PostgresClient;
+
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -23,7 +28,7 @@ public class Crawler {
         String dbUser = System.getenv("DB_USER");
         String dbPass = System.getenv("DB_PASSWORD");
         String dbName = System.getenv("DB_NAME");
-
+       // String dbPort = System.getenv("DB_PORT");
 
         if (dbHost == null || dbUser == null || dbPass == null || dbName == null || dbHost.isBlank() || dbUser.isBlank()
                 || dbPass.isBlank() || dbName.isBlank()) {
@@ -57,8 +62,10 @@ public class Crawler {
         timer.scheduleAtFixedRate(timerTask, 1000, 5 * 60 * 1000);
         System.out.println("FetchAndStoreTask started");
 
-        final ContextHandler context = new ContextHandler("/health");
-        context.setHandler(new AbstractHandler() {
+        final ContextHandler health = new ContextHandler("/health");
+        final ContextHandler api = new ContextHandler("/api");
+        final ContextHandler pingpong = new ContextHandler("/api/pingpong");
+        health.setHandler(new AbstractHandler() {
             @Override
             public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException {
                 response.getWriter().println("UP");
@@ -66,9 +73,52 @@ public class Crawler {
             }
         });
 
+        api.setHandler(new AbstractHandler() {
+            @Override
+            public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException {
+                response.getWriter().println("this is api");
+                baseRequest.setHandled(true);
+            }
+        });
+        pingpong.setHandler(new AbstractHandler() {
+            @Override
+            public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+                String echo = "";
+
+                if(request.getParameter("echo") != null){
+                    echo = request.getParameter("echo");
+                }
+                if(!echo.isEmpty()){
+
+                    if(echo.equals("ping")){
+
+                        response.getWriter().println("pong");
+
+                    } else if(echo.equals("pong")){
+
+                        response.getWriter().println("ping");
+
+                    }  else {
+
+
+                        response.setStatus(400);
+                    }
+                } else {
+
+                    response.getWriter().println("please enter a parameter ");
+
+                }
+                baseRequest.setHandled(true);
+
+            }
+
+        });
+
+        ContextHandlerCollection contexts = new ContextHandlerCollection(health, api, pingpong);
         final String port = System.getenv().getOrDefault("PORT", "8888");
         final Server server = new Server(Integer.parseInt(port));
-        server.setHandler(context);
+        server.setHandler(contexts);
         server.start();
         server.join();
     }
